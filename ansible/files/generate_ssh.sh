2b2a9d712719b569fd1bcad3543853d0ba0caf16
Emil Lubikowski <e.lubikowski@gmail.com>
2021-03-28 23:33:40 +0200

    small improvement for ssh key pair generation

--- ansible/files/generate_ssh.sh
+++ ansible/files/generate_ssh.sh
@@ -15,10 +15,39 @@ echo -n AWS region:
 read -s AWS_DEFAULT_REGION
 echo
 
+echo "Do you want to deploy ssh key to AWS? true/yes to accept, any value to skip"
+read DEPLOY_KEY
+
+if [[ $DEPLOY_KEY = 'true' ]] || [[ $DEPLOY_KEY = 'yes' ]]
+then
+  echo "ensuring that local time Zone is set to proper value - Europe/Warsaw"
+  sudo timedatectl set-timezone 'Europe/Warsaw'
+
+  echo "type name for new ssh key pair:"
+  read SSH_NAME
+
+  echo "generating new ssh key pair"
+  ssh-keygen -t rsa -N "" -C "$SSH_NAME" -f ~/.ssh/id_rsa.pem
+
+  echo "deploy ssh to aws"
+   aws ec2 import-key-pair --key-name "$SSH_NAME" --public-key-material fileb://~/.ssh/id_rsa.pem.pub
+
+  echo "set permission 600 to ssh key"
+  chmod 600 .ssh/id_rsa.pem
+
+  echo "add key to ssh-agent"
+  ssh-add ~/.ssh/id_rsa
+  echo "ssh-add ~/.ssh/id_rsa" >> ~/.bashrc
+
+  echo "credentials.tfvars settings updated with new key pair name"
+  sed -i "s/AWS_SSH_KEY_NAME.*/AWS_SSH_KEY_NAME = \"$SSH_NAME\"/g" ~/deployments/kubespray/contrib/terraform/aws/credentials.tfvars
+else
+  echo "Key deploy skipped"
+fi
+
 echo "update credentials.tfvars settings"
 sed -i "s/AWS_ACCESS_KEY_ID.*/AWS_ACCESS_KEY_ID = \"$AWS_ACCESS_KEY_ID\"/g" ~/deployments/kubespray/contrib/terraform/aws/credentials.tfvars
 sed -i --expression "s@AWS_SECRET_ACCESS_KEY.*@AWS_SECRET_ACCESS_KEY = \"$AWS_SECRET_ACCESS_KEY\"@g" ~/deployments/kubespray/contrib/terraform/aws/credentials.tfvars
-sed -i "s/AWS_SSH_KEY_NAME.*/AWS_SSH_KEY_NAME = \"deployment-key\"/g" ~/deployments/kubespray/contrib/terraform/aws/credentials.tfvars
 sed -i "s/AWS_DEFAULT_REGION.*/AWS_DEFAULT_REGION = \"$AWS_DEFAULT_REGION\"/g" ~/deployments/kubespray/contrib/terraform/aws/credentials.tfvars
 
 echo "create aws config files for ssh deployment"
@@ -33,29 +62,5 @@ cat <<EOT > ~/.aws/credentials
 aws_access_key_id=$AWS_ACCESS_KEY_ID
 aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
 EOT
-
-echo "Do you want to deploy ssh key to AWS? true/yes to accept, any value to skip"
-read DEPLOY_KEY
-
-if [[ $DEPLOY_KEY = 'true' ]] || [[ $DEPLOY_KEY = 'yes' ]]
-then
-  echo "ensure local time Zone is set to proper value - Europe/Warsaw"
-  sudo timedatectl set-timezone 'Europe/Warsaw'
-
-  echo "generating new ssh key pair"
-  ssh-keygen -t rsa -N "" -C "deployment-key" -f ~/.ssh/id_rsa
-
-  echo "deploy ssh to aws"
-   aws ec2 import-key-pair --key-name "deployment-key" --public-key-material fileb://~/.ssh/id_rsa.pub
-
-  echo "set permission 600 to ssh key"
-  chmod 600 .ssh/id_rsa
-
-  echo "add key to ssh-agent"
-  ssh-add ~/.ssh/id_rsa
-  echo "ssh-add ~/.ssh/id_rsa" >> ~/.bashrc
-else
-  echo "Key deploy skipped"
-fi
 
 echo "done"
